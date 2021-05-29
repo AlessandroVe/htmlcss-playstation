4d814b7ed549975c84725bc39840c6ca6e489b26
AlessandroVe <aveneziano536@gmail.com>
2021-05-29 10:59:52 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var y = "Hello Word";
+var x = "Giochi incredibili live su PS4, con 1 TB  di spazio di archiviazione";
+console.log("HELLO WORD");
+document.getElementById("show-ps4-title").innerHTML="Esplora PlayStation® PS4";
+document.getElementById("section-p").innerHTML= x;
+alert(y);
